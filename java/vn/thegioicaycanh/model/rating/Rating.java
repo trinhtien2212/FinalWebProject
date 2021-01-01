0300bb4bf5d13b8583d98160fa9a1447fc1648b0
duongbanhuan2020 <duongbanhuan2020@gmail.com>
2021-01-01 08:58:50 +0700

    add file

---
+++ java/vn/thegioicaycanh/model/rating/Rating.java
@@ -0,0 +1,63 @@
+package vn.thegioicaycanh.model.rating;
+
+import java.util.Date;
+
+public class Rating {
+    private int id;
+    private int user_id;
+    private int pro_id;
+    private String comment;
+    private int rating_type_id;
+    private Date date_created;
+
+    public Rating() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getPro_id() {
+        return pro_id;
+    }
+
+    public void setPro_id(int pro_id) {
+        this.pro_id = pro_id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getRating_type_id() {
+        return rating_type_id;
+    }
+
+    public void setRating_type_id(int rating_type_id) {
+        this.rating_type_id = rating_type_id;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+}
---
+++ java/vn/thegioicaycanh/model/warranty/warranty.java
@@ -0,0 +1,81 @@
+package vn.thegioicaycanh.model.warranty;
+
+import java.util.Date;
+
+public class warranty {
+    private int id;
+    private int order_id;
+    private int user_id;
+    private int pro_id;
+    private String message;
+    private String img;
+    private Date date_created;
+    private int status;
+
+    public warranty() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getPro_id() {
+        return pro_id;
+    }
+
+    public void setPro_id(int pro_id) {
+        this.pro_id = pro_id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}

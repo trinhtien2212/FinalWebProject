bac86b82d4770a745f3d134629e6debac7b6ce85
duongbanhuan2020 <duongbanhuan2020@gmail.com>
2021-01-03 08:20:18 +0700

    add file

--- java/vn/thegioicaycanh/controller/user_page/Shopping_sorted_By_Date_Direct.java
+++ java/vn/thegioicaycanh/controller/user_page/Shopping_sorted_By_Date_Direct.java
@@ -1,6 +1,7 @@
 package vn.thegioicaycanh.controller.user_page;
 
 import vn.thegioicaycanh.model.Product.ProductEntity;
+import vn.thegioicaycanh.model.home_page.Home_page;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,6 +17,10 @@ public class Shopping_sorted_By_Date_Direct extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("page_menu","shopping");
+        request.setAttribute("title","mua sắm");
+        request.setAttribute("home_page_data",new Home_page());
+        request.setAttribute("new_pros",ProductEntity.loadNewProducts(9));
         request.setAttribute("shopping_grid_data", ProductEntity.loadNewProducts(9));
         request.getRequestDispatcher("user_page/shopping-grid.jsp").forward(request,response);
     }

2d317cf060689b5a4a7317302eda45af6d0c320b
duongbanhuan2020 <duongbanhuan2020@gmail.com>
2021-01-06 22:10:21 +0700

    add file

--- java/vn/thegioicaycanh/controller/user_page/Blog_direct.java
+++ java/vn/thegioicaycanh/controller/user_page/Blog_direct.java
@@ -21,12 +21,26 @@ public class Blog_direct extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         initAttr();
+        int pages = 1;
+        String sqlCondition="";
+        String url="";
+        if(request.getParameter("pages") != null) {
+            pages = Integer.parseInt(request.getParameter("pages"));
+        }
+        request.setAttribute("sql","select * from blog "+sqlCondition);
+        request.setAttribute("sumOfItems_sql","select count(*) from blog "+sqlCondition);
+        request.setAttribute("numOfItemLoad",10);
         request.setAttribute("page_menu","blog");
+//        request.setAttribute("pages",pages);
+        request.setAttribute("type_page","blog");
+        request.setAttribute("direct_to","user_page/blog.jsp");
         request.setAttribute("title","Bài viết");
         request.setAttribute("home_page_data",new Home_page());
-        request.setAttribute("blog_data", Blog_Con_DB.loadLimitBlog(1,10));
+
+        request.setAttribute("url",url);
+//        request.setAttribute("data", Blog_Con_DB.loadLimitBlog(1,10));
         request.setAttribute("newBlog",Blog_Con_DB.loadNewBlogs(12));
-        request.getRequestDispatcher("user_page/blog.jsp").forward(request,response);
+        request.getRequestDispatcher("handlePagination").forward(request,response);
 
     }
     protected void initAttr(){

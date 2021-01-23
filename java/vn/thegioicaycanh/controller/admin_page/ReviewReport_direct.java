26da3c28ecfd15ddae3e00bbbc0b90c8d26c3436
duongbanhuan2020 <duongbanhuan2020@gmail.com>
2021-01-23 11:25:32 +0700

    add file

---
+++ java/vn/thegioicaycanh/controller/admin_page/ReviewReport_direct.java
@@ -0,0 +1,20 @@
+package vn.thegioicaycanh.controller.admin_page;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/admin_page/reviewreport")
+public class ReviewReport_direct extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("current_page","review-report");
+        request.getRequestDispatcher("review-reports.jsp").forward(request,response);
+    }
+}
